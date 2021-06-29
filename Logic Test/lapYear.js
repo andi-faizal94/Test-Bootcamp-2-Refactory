4959d52db28ada72e7d58dbe91a2843fe8489c56
andi-faizal94 <andifaizal647@gmail.com>
2021-06-29 12:53:42 +0700

    menambahkan file baru

--- Logic Test/lapYear.js
+++ Logic Test/lapYear.js
@@ -1,19 +1,12 @@
 function lapYear(year) {
-    // this.year1 = year1;
-    // this.year2 = year2;
-
-    for (i = 0; i <= year.length; i++) {
-
-
-        if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
-            console.log(i)
-            return true
-        } else {
-            console.log("error")
-            return false
-        }
-    }
-    return false
+    return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
 }
 
-console.log(lapYear(1990, 2020));
+let a = 1900;
+let b = 2020;
+
+for (a; a <= b; a++) {
+    if (lapYear(a)) {
+        console.log(a)
+    }
+}
